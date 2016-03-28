09cb8529150799ea8302fbfe664a15a92fd6a545
hobart.wang <hobart.cn@gmail.com>
2016-03-28 23:02:46 +0800

    表单提交
    form 会用到三个 Material UI 组件：button ，card 和 text field 。

--- src/Account.js
+++ src/Account.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+import TextField from 'material-ui/lib/text-field';
+import Card from 'material-ui/lib/card/card';
 
 class Account extends Component {
-  render() {
-    return (
-      <div className='account'>
-        account page here
-      </div>
-    );
-  }
+	_handleSubmit(e){
+		e.preventDefault();
+		const account = this.refs.account.getValue();
+		console.log(account);
+	}
+	render() {
+	return (
+		<div className="account">
+		<Card className="content">
+			<form onSubmit={this._handleSubmit.bind(this)}>
+				<TextField hintText="Your Github Account"
+					ref="account"/>
+				<FlatButton label="Search Github"
+					type="submit"
+					primary={true}/>
+			</form>
+		</Card>
+		</div>
+	);
+	}
 }
 
 export default Account;
