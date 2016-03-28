c6c3cf416b1ed0298a4e3962033e1e74a5759e28
hobart.wang <hobart.cn@gmail.com>
2016-03-28 21:28:10 +0800

    使用 react-router
    使用 react-router 实现“多页面”的单页面应用
    cnpm i -D react-router@^2.0.0-rc4

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
-// import { App } from './App';
-import Point from './Point';
+import App from './App';
 import './style/main.scss';
 
+import { Router, browserHistory } from 'react-router'
+import routes from './routes';
 
-render(<Point />, document.getElementById('root'));
+render(<Router history={browserHistory}>{routes}</Router>
+, document.getElementById('root'));
 
