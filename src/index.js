54033a49762388e9f3c348994a6afb5cb53825de
hobart.wang <hobart.cn@gmail.com>
2016-03-28 15:50:41 +0800

    添加一个 ES6 模块实例

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import Point from './Point';
 
 render(<App />, document.getElementById('root'));
+
+var pnt = new Point(2,3);
+console.log(pnt.toString());
