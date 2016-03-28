a9035f655cab4d182d49d030e9d2cf3c4b27db33
hobart.wang <hobart.cn@gmail.com>
2016-03-28 17:23:56 +0800

    使用 material-ui 的 Tabs 组件
    cnpm i -D material-ui react-tap-event-plugin

--- src/Point.js
+++ src/Point.js
@@ -1,12 +1,13 @@
 import React ,{ Component } from 'react';
 import Home from './Home';
+import NavBar from './shared/NavBar';
 
 class Point extends Component
 {
 	render() {
 		return (
 			<div className="app-wrap">
-				<div className="app-header"></div>
+				<NavBar />
 				<Home />
 				<div className="app-footer">My Footer</div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 // import { App } from './App';
 import Point from './Point';
 import './style/main.scss';
+
 
 render(<Point />, document.getElementById('root'));
 
